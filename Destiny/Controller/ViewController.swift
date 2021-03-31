6bfba19a496be675e7de023d6f1fe312562fc66d
Adnan Sarker <adnansarker92@gmail.com>
2021-03-31 23:30:54 +0600

    add stories and nicely distributed all coding logic in MVC  format

--- Destiny/Controller/ViewController.swift
+++ Destiny/Controller/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyText: UILabel!
+    @IBOutlet weak var choiceOneButton: UIButton!
+    @IBOutlet weak var choiceTwoButton: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyText.text = storyBrain.getStoryTitle()
+        choiceOneButton.setTitle(storyBrain.getChoice1(), for: .normal)
+        choiceTwoButton.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
